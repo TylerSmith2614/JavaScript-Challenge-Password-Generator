4b350c593b2053e90cbc1c856aae5ae53a5a12a3
Smith <tylersmith2614@gmail.com>
2023-03-11 20:54:44 -0700

    created variables, if statements for prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,26 @@
 // Assignment code here
 var characterLength = 8;
-var choiceArray = ""
+var choiceArr = [];
 var specialChars= ["!", "@","#","$","%","^","&","*","(",")","_","+","{","}","[","]","?"];
 var numberChars = ["0","1","2","3","4","5","6","7","8","9"];
 var lcChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-
-// Step 0: Define the function generatePassword.
-function generatePassword(){
-  console.log("It's working! You clicked the button!");
-  // Step 1: Prompt the user for the password criteria inside of the generateButton function. 
-  characterLength = parseInt(prompt ("How many characters would you like your password to contain?", "Password must be between 8-128 characters"));
-  if (confirm ("Click ok to confirm including special characters.",)) {
-    choiceArray = choiceArray.concat(specialChars);
-  }
-  if (confirm ("Click ok to confirm including numbers.")) {
-    choiceArray = choiceArray.concat(numberChars);
-  }
-  if (confirm ("Click ok to confirm including lowercase letters.")) {
-    choiceArray = choiceArray.concat(lcChars);
-  }
-  if (confirm ("Click ok to confirm including uppercase letters.")) {
-    choiceArray = choiceArray.concat(upChars);
-  }
-
-// Step 2: Validate the input based on the criteria
- if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-    alert("Character length input must be a number between 8-128 characters. Please try again.");
-    return "Please try again";
- }
- 
-  // Step 3: Generate password based on criteria.
-
-// Step 4:Display the password in the textarea by returning the function with the var set to the generated password.
-return true
-// }
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
+  var correctPrompts = getPrompts();
+
+  if(correctPrompts) {
+    
+  }
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -49,5 +28,30 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+}
+
+  function getPrompts(){
+    choiceArr = [];
+    characterLength = parseInt(prompt ("How many characters would you like your password to contain?", "Password must be between 8-128 characters"));
+
+      if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+        alert("Charcter lengths has to be between 8-128 characters. Please Try again.");
+        return false;
+      }
+
+      if (confirm("Would you like lowercase letters in your password?")) {
+        choiceArr = choiceArr.concat(lcChars);
+      }
+      if (confirm("Would you like uppercase letters in your password?")) {
+        choiceArr = choiceArr.concat(upChars);
+      }
+      if (confirm("Would you like special characters in your password?")) {
+        choiceArr = choiceArr.concat(specialChars);
+      }
+      if (confirm("Would you like number in your password?")) {
+        choiceArr = choiceArr.concat(numberChars);
+      }
+      return true;
+  }
